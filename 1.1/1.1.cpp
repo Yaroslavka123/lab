f50aedc75d209096dff2ea1e14b538497816262b
unknown <login12311@tut.by>
2023-09-18 00:59:59 +0300

    1.1 add

---
+++ 1.1/1.1.cpp
@@ -0,0 +1,13 @@
+﻿
+using namespace std;
+#include <iostream>
+
+int main()
+{
+    float x, t, s, result, comp;
+    cin >> x;
+    cin >> t;
+    cin >> s;
+    comp = pow(pow(x, 3) + 1, 1 / 2) + log(s);;
+    cout << pow(x, t) / comp;
+}
