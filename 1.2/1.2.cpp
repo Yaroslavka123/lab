f8068b8fead6f33aa305cee2343bf9a67664fce4
unknown <login12311@tut.by>
2023-09-18 01:23:54 +0300

    1.2 add

---
+++ 1.2/1.2.cpp
@@ -0,0 +1,22 @@
+﻿using namespace std;
+#include <iostream>
+
+int main()
+{
+    float x, y, r;
+    cin >> x;cin >> y;cin >> r;
+    if ((x > 0 && y > 0) || (x < 0 && y < 0)) {
+        cout << "outside the scope ";
+        return 0;
+    }
+    if (x >= -r && pow(x+r,2)+ pow(y-r,2) >= pow(r,2))  {
+        cout << "true";
+        return 0;
+     }
+    if (x <= r &&  x >= 0 && pow(x,2 ) + pow(y,2) <= pow(r,2)) {
+        cout << "true";
+        return 0;
+    }
+    cout << "false";
+}
+
